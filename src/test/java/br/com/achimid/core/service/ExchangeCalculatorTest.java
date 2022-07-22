37ec61e39ef2452004203964ed994f1a40232502
Lourran Olimpio Zorzi <achimid@hotmail.com>
2022-07-22 11:09:10 -0300

    Including some unit tests

--- src/test/java/br/com/achimid/core/service/ExchangeCalculatorTest.java
+++ src/test/java/br/com/achimid/core/service/ExchangeCalculatorTest.java
@@ -1,6 +1,46 @@
 package br.com.achimid.core.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import br.com.achimid.core.domain.exchange.ExchangeEntity;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static java.math.BigDecimal.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@MicronautTest
 class ExchangeCalculatorTest {
-  
+
+    @Inject
+    private ExchangeCalculator exchangeCalculator;
+
+    @Test
+    void should_calculate_source_value_with_success() {
+        final var exchange = ExchangeEntity.builder().sourceValue(TEN).build();
+        final var rate = new BigDecimal("1.5");
+
+        final var calculated = exchangeCalculator.calculate(exchange, rate);
+
+        assertEquals(valueOf(15).setScale(2), calculated.getTargetValue());
+        assertEquals(rate, calculated.getRate());
+    }
+
+    @Test
+    void should_calculate_with_null_exchange() {
+        assertThrows(NullPointerException.class, () -> exchangeCalculator.calculate(null, null));
+    }
+
+    @Test
+    void should_calculate_with_null_rate() {
+        assertThrows(NullPointerException.class, () -> exchangeCalculator.calculate(ExchangeEntity.builder().build(), null));
+    }
+
+    @Test
+    void should_calculate_with_null_source_value() {
+        assertThrows(NullPointerException.class, () -> exchangeCalculator.calculate(ExchangeEntity.builder().build(), ZERO));
+    }
+
 }
--- src/test/java/br/com/achimid/core/service/ExchangeRatesServiceTest.java
+++ src/test/java/br/com/achimid/core/service/ExchangeRatesServiceTest.java
@@ -1,6 +1,36 @@
 package br.com.achimid.core.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import br.com.achimid.infra.client.LayerApiClient;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.test.annotation.MockBean;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static io.micronaut.core.util.StringUtils.FALSE;
+import static io.micronaut.core.util.StringUtils.TRUE;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@MicronautTest
+@Requires(property = "mockito.test.enabled", defaultValue = FALSE, value = TRUE)
 class ExchangeRatesServiceTest {
-  
+
+    @Inject
+    ExchangeRatesService exchangeRatesService;
+
+
+    @Test
+    void should_test_rate() {
+        final var rates = exchangeRatesService.getRates();
+
+        assertNotNull(rates);
+    }
+
+    @MockBean(LayerApiClient.class)
+    LayerApiClient mathService() {
+        return Mockito.mock(LayerApiClient.class);
+    }
+
+
 }
