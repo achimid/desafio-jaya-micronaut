7c26a410b022243865bcfb5ab923abc648a17283
Lourran Olimpio Zorzi <achimid@hotmail.com>
2022-07-22 11:06:00 -0300

    Adding mapper from structMapper

--- src/main/java/br/com/achimid/core/mapper/ExchangeMapper.java
+++ src/main/java/br/com/achimid/core/mapper/ExchangeMapper.java
@@ -1,2 +1,22 @@
-package br.com.achimid.core.mapper;public interface ExchangeMapper {
+package br.com.achimid.core.mapper;
+
+import br.com.achimid.api.web.request.ExchangeRequest;
+import br.com.achimid.api.web.response.ExchangeResponse;
+import br.com.achimid.core.domain.exchange.ExchangeEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.time.LocalDateTime;
+
+import static org.mapstruct.MappingConstants.ComponentModel.JAKARTA;
+import static org.mapstruct.ReportingPolicy.IGNORE;
+
+@Mapper(unmappedTargetPolicy = IGNORE, componentModel = JAKARTA, imports = {LocalDateTime.class})
+public interface ExchangeMapper {
+
+    @Mapping(target = "createdAt", expression = "java(LocalDateTime.now())")
+    ExchangeEntity from(ExchangeRequest request);
+
+    ExchangeResponse from(ExchangeEntity entity);
+
 }
