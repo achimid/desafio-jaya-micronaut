3d9b74d1872b1da2715ddb0deb918093c7680c02
Lourran Olimpio Zorzi <achimid@hotmail.com>
2022-07-22 11:08:33 -0300

    Adding service to calculate values

--- src/main/java/br/com/achimid/core/service/ExchangeCalculator.java
+++ src/main/java/br/com/achimid/core/service/ExchangeCalculator.java
@@ -1,4 +1,31 @@
 package br.com.achimid.core.service;
 
+import br.com.achimid.core.domain.exchange.ExchangeEntity;
+import jakarta.inject.Singleton;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+import java.math.BigDecimal;
+
+import static java.math.RoundingMode.HALF_EVEN;
+import static java.util.Objects.requireNonNull;
+
+@Slf4j
+@Singleton
 public class ExchangeCalculator {
+
+    public ExchangeEntity calculate(@NonNull final ExchangeEntity exchange, @NonNull final BigDecimal rate) {
+        log.info("Calculating exchange values. rate={}", rate);
+
+        final var sourceValue = requireNonNull(exchange.getSourceValue());
+        final var targetValue = rate.multiply(sourceValue).setScale(2, HALF_EVEN);
+
+        final var exchangeCalculated = exchange.toBuilder()
+                .targetValue(targetValue)
+                .rate(rate)
+                .build();
+
+        return exchangeCalculated;
+    }
+
 }
