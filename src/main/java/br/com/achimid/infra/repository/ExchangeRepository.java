7c93c2b7b3a1526d3bd42e6651b10ad5733f6597
Lourran Olimpio Zorzi <achimid@hotmail.com>
2022-07-22 11:08:18 -0300

    Crating repository for exchange domain

--- src/main/java/br/com/achimid/infra/repository/ExchangeRepository.java
+++ src/main/java/br/com/achimid/infra/repository/ExchangeRepository.java
@@ -1,2 +1,14 @@
-package br.com.achimid.infra.repository;public interface ExchangeRepository {
+package br.com.achimid.infra.repository;
+
+import br.com.achimid.core.domain.exchange.ExchangeEntity;
+import io.micronaut.data.annotation.Repository;
+import io.micronaut.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+@Repository
+public interface ExchangeRepository extends JpaRepository<ExchangeEntity, Long> {
+
+    List<ExchangeEntity> findByUserId(String userId);
+
 }
